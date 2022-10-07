07c48eb07748b5a18ef86991fdd25f650853c142
eri0406 <anpanman.0607.ibu@gmail.com>
2022-10-07 23:22:03 +0900

    トップページからログイン、新規登録画面に遷移

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  validates :nickname, presence: true
+  validates :birth_day, presence: true
+  validates :encrypted_password, presence:true
+  with_options presence: true do
+  validates :last_name, format: {with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: "is invalid. Input full-width characteres."}
+  validates :first_name, format: {with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: "is invalid. Input full-width characteres."}
+  validates :read_last_name, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :read_first_name, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  end
 end
