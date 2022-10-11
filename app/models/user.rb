a4da27c070a83b0f4d191d2a380c19e9a5b878d3
eri0406 <anpanman.0607.ibu@gmail.com>
2022-10-11 17:25:27 +0900

    validates :encrypted_password, presence:trueの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   validates :birth_day, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-  validates :encrypted_password, presence:true
   with_options presence: true do
     validates :last_name, format: {with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: "is invalid. Input full-width characteres."}
     validates :first_name, format: {with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: "is invalid. Input full-width characteres."}
