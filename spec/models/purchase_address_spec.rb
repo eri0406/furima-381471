08f2d89db104580588d69e1d68333979da0485d2
eri0406 <anpanman.0607.ibu@gmail.com>
2022-10-28 21:21:44 +0900

    単体テストコードの実施

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe PurchaseAddress, type: :model do
 
     context '内容に問題がない場合' do
       it '全ての値が正しく入力されていれば保存できること' do
-        binding.pry
         expect(@purchase_address).to be_valid
       end
 
@@ -24,51 +23,55 @@ RSpec.describe PurchaseAddress, type: :model do
       it 'postal_codeが空だと保存できないこと' do
         @purchase_address.postal_code = ''
         @purchase_address.valid?
-        
+        expect( @purchase_address.errors.full_messages).to include("Postal code can't be blank")
       end
 
       it 'postal_codeが「3桁ハイフン4桁」の半角文字列でないと保存できないこと' do
         @purchase_address.postal_code = '1234567'
         @purchase_address.valid?
-
+        expect( @purchase_address.errors.full_messages).to include("Postal code is invalid")
       end
 
-      it 'prefecture_idを選択していないと保存できないこと' do
+      it 'prefecture_idの1以外を選択していないと保存できないこと' do
         @purchase_address.prefecture_id = 1
-        @prefecture_address.valid?
-
+        @purchase_address.valid?
+        expect( @purchase_address.errors.full_messages).to include("Prefecture must be other than 1")
       end
 
       it 'cityが空だと保存できないこと' do
         @purchase_address.city = ''
         @purchase_address.valid?
-
+        expect( @purchase_address.errors.full_messages).to include("City can't be blank")
       end
 
       it 'house_numberが空だと保存できないこと' do
         @purchase_address.house_number = ''
         @purchase_address.valid?
-
+        expect( @purchase_address.errors.full_messages).to include("House number can't be blank")
       end
 
       it 'phone_numberが空だと保存できないこと' do
         @purchase_address.phone_number = ''
         @purchase_address.valid?
-
+        expect( @purchase_address.errors.full_messages).to include("Phone number can't be blank")
       end
 
       it 'phone_numberが9桁以下では保存ができないこと' do
-
+        @purchase_address.phone_number = '090123456'
+        @purchase_address.valid?
+        expect( @purchase_address.errors.full_messages).to include("Phone number is invalid")
       end
 
       it 'phone_numberが12桁以上では保存ができないこと' do
-
+        @purchase_address.phone_number = '090123456789'
+        @purchase_address.valid?
+        expect( @purchase_address.errors.full_messages).to include("Phone number is invalid")
       end
 
       it 'userが紐付いていないと保存できないこと' do
         @purchase_address.user_id = nil
         @purchase_address.valid?
-
+        expect( @purchase_address.errors.full_messages).to include("User can't be blank")
       end
     end
   end
